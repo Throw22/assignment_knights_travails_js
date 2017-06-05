6ee27bf1e235dd881f631dad62d79bbb734b73a7
Nicholas Romeo <romeonicholas@gmail.com>
2017-06-05 10:37:49 -0700

    Work on tree building

--- knight.js
+++ knight.js
@@ -1,5 +1,5 @@
 class Move {
-  constructor(x, y, depth = 0, children = null, parent = null) {
+  constructor(x, y, depth = 0, children = [], parent = null) {
     this.x = x;
     this.y = y;
     this.depth = depth;
@@ -13,7 +13,39 @@ class MoveTree {
     this.start = new Move(start[0], start[1]);
     this.maxDepth = maxDepth;
     this.counter = 0;
+    this.buildTree(start, maxDepth);
   }
+
+  // Given starting location and depth
+  // Add 8 moves to node based off given location
+  //
+
+  buildTree(start, maxDepth) {
+    let currentNode = this.start;
+
+    if (!currentNode.children.length) {
+      // for (let i = 1; i <= maxDepth; i++) {
+      //this.counter += 8;
+      currentNode.children.push(
+        new Move(currentNode.x + 2, currentNode.y + 1, i, [], currentNode),
+        new Move(currentNode.x + 2, currentNode.y - 1, i, [], currentNode),
+        new Move(currentNode.x + 1, currentNode.y + 2, i, [], currentNode),
+        new Move(currentNode.x - 1, currentNode.y + 2, i, [], currentNode),
+        new Move(currentNode.x - 2, currentNode.y + 1, i, [], currentNode),
+        new Move(currentNode.x - 2, currentNode.y - 1, i, [], currentNode),
+        new Move(currentNode.x + 1, currentNode.y - 2, i, [], currentNode),
+        new Move(currentNode.x - 1, currentNode.y - 2, i, [], currentNode)
+      );
+      //  currentNode =
+      //  }
+
+      currentNode.children.forEach(child => {});
+    }
+  }
+
+  // addMove() {
+  //
+  // }
 
   inspect() {
     console.log('maxDepth', this.maxDepth, '# nodes', this.counter);
